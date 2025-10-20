4944ed1e803aa3af2e5834224017bda82aa57ce5
Evan Keller <33649ek@gmail.com>
2025-10-20 10:42:22 -0600

    singly linked list

--- Double-Pointer-Fundamentals/singly_linked_list.c
+++ Double-Pointer-Fundamentals/singly_linked_list.c
@@ -1,12 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct Node {
+struct Node
+{
     int x;
     struct Node *next;
 };
 
+// Need double pointer for head node
+void push_front(struct Node **head, int value)
+{
+    // struct Node new_head = {value, *head};
+    // *head = &new_head;
 
-int main() {
+    struct Node *new = malloc(sizeof *new);
+    if (!new)
+    {
+        perror("Malloc");
+        exit(1);
+    }
+
+    new->x = value;
+    new->next = *head;
+    *head = new;
+}
+
+int main(void)
+{
+
+    struct Node list = {12, NULL};
+    struct Node *head = &list;
+
+    int new_value = 49;
+
+    push_front(&head, new_value);
+
+    struct Node *next_node = list.next;
+    struct Node *current_node = &list;
+    while (current_node != NULL)
+    {
+        printf("%d ", current_node->x);
+        current_node = next_node;
+        next_node = current_node->next;
+    }
 
     return 0;
 }
